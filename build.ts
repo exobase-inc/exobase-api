a8f97c20fc170ba581cd1f1356adaec6d1e73eaa
Ray Epps <rayharryepps@gmail.com>
2022-07-24 22:23:33 -0600

    make build generic with manifest

--- build.ts
+++ build.ts
@@ -4,34 +4,50 @@ import webpack from 'webpack'
 import TerserPlugin from 'terser-webpack-plugin'
 import cmd from 'cmdish'
 import glob from 'glob'
+import fs from 'fs'
 
 const start = async () => {
-  await cmd('rm -rf ./build')
   const functions = glob.sync('src/modules/**/*.ts')
+  await cmd('rm -rf ./build')
   const [first, ...rest] = functions
   // Build one first so if there is an error our
   // machine doesn't spin out of control like a
-  // nuclear meltdown building 30+ functions if
-  // if they all have errors.
+  // nuclear meltdown building 30+ functions that
+  // all have errors.
   await compile(first)
   await _.parallel(6, rest, compile)
+  await cmd('rm ./.manifest.json')
+  await manifest(functions)
 }
 
-function compile(funcPath: string) {
-  const [_x, module, func] = funcPath.match(/\/([a-zA-Z_-]+?)\/([a-zA-Z_-]+?)\.ts/) as string[]
-  console.log(`transpiling ${module}/${func}`)
+const manifest = async (functions: string[]) => {
+  const content = {
+    functions: functions.map(p => ({
+      source: p.replace(/src\/modules\//, 'build/').replace(/\.ts$/, '.js'), // => build/health/ping.js
+      url: p.replace(/^.+modules\//, '/').replace(/\.ts$/, '') // => /health/ping
+    }))
+  }
+  await fs.promises.writeFile(
+    './.manifest.json',
+    JSON.stringify(content, null, 2),
+    'utf-8'
+  )
+}
+
+const compile = async (func: string) => {
+  console.log(`building ${func.replace(/src\/modules/, '')}`)
   return new Promise<void>((res, rej) => {
     webpack(
       {
-        entry: [`./src/modules/${module}/${func}.ts`],
+        entry: [`./${func}`],
         mode: (process.env.NODE_ENV as 'production' | 'development') ?? 'production',
         target: 'async-node14',
         output: {
           library: {
             type: 'commonjs2'
           },
-          path: path.resolve(__dirname, 'build', 'modules', module),
-          filename: `${func}.js`
+          path: path.resolve(__dirname, 'build', func.replace(/^src\/modules\//, '').replace(/\/([a-zA-Z_-]+)\.ts$/, '')),
+          filename: func.replace(/\.ts$/, '.js').replace(/(.+)\//, '')
         },
         resolve: {
           extensions: ['.ts', '.js']
